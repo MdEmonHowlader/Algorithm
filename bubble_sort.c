17cf95d63feb1bcb330e11bf7d6b1793d9622ae7
Emon <emon22205101732@diu.edu.bd>
2024-01-24 23:50:02 +0600

    Bubble

---
+++ bubble_sort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a[]={3,13,4,10,6,2,7,9,0,8,78};
+    int b=10;
+    for(int i=0; i<b; i++){
+
+        for(int j=0; j<(b-1); j++){
+          if(a[j]>a[j+1]){
+              int temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+          }
+
+        }
+    }
+    for(int i=0; i<=b; i++){
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+}
--- onetest.c
+++ onetest.c
@@ -1,4 +1,18 @@
 #include<stdio.h>
-int main(){
-    printf("Hi Algorithm");
-}
+ int main(){
+    int n[5]={23,45,67,89,22};
+    int a;
+    int b=0;
+    scanf("%d", &a);
+     for(int i=0; i<=5; i++){
+      printf("%d\n", n[i]);
+      if(n[i]==a){
+        b=1;
+        break;
+      }
+     } if(b){
+         printf("Found");
+      }else{
+         printf("Not Found");
+      } 
+ }
