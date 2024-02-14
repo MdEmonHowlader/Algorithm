ab2c2f8cbe8e85ccb1ccfb5825d5e7570e7d26a9
Emon <emon22205101732@diu.edu.bd>
2024-02-14 22:52:58 +0600

    LAB

--- labReport7.c
+++ labReport7.c
@@ -1,57 +1,27 @@
-// Error
-#include<stdio.h>
-int main(){
-    int n;
+#include <stdio.h> 
+int main() {
+    int n; 
     scanf("%d", &n);
-    int e1[n+1];
-    int e2[n];
-    int e[n-1];
-    for(int i=1; i<n; i++){
-        scanf("%d", &e1[i]);
+
+    int  a[n], b[n-1], c[n-2];
+    int sum1=0, sum2=0, sum3=0;
+    int count1, count2;
+
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+        sum1 += a[i];
     }
-    for(int i=1; i<n; i++){
-        scanf("%d", &e2[i]);
+    for(int i=0; i<n-1; i++){
+        scanf("%d", &b[i]);
+        sum2 += b[i];
     }
-    for(int i=1; i<n; i++){
-        scanf("%d", &e[i]);
+    for(int i=0; i<n-2; i++){
+        scanf("%d", &c[i]);
+        sum3+=c[i];
     }
-    int a=0;
-    for(int i=1; i<n; i++){
-        int b1=0;
-        int b2=0;
-        for(int j=1; i<n; j++){
-            if(e1[i]==e2[j]){
-                b1=1;
-            }
-        }
-        for(int j=1; j<n-1; j++){
-            if(e1[i]==e[j]){
-                b2=1;
-            }
-        }
-        if(!b1&&b2){
-            a=e1[i];
-            break;
-        }
-    }
-    int a2=0;
-    for(int i=1; i<n; i++){
-        int c1=0;
-        int c2=0;
-        for(int j=1; j<n-1; j++){
-            if(e2[i]==e[j]){
-                c1=1;
-            }
-        }
-        for(int j=1; j<n-2; j++){
-            if(e2[i]==e[j]){
-                c2=1;
-            }
-        }
-        if(!c1 && c2){
-            a2=e2[i];
-            break;
-        }
-    }
-    printf("%d %d\n", a, a2);
+    count1=sum1-sum2;
+    count2=sum2-sum3;
+
+    printf("%d %d\n", count1, count2);
 }
--- test.c
+++ test.c
@@ -1,6 +1,28 @@
-    #include <stdio.h>
-    int main(){
-        printf("/");
+#include <stdio.h> 
+int main() {
+    int n; 
+    scanf("%d", &n);
 
+    int  a[n], b[n-1], c[n-2];
+    int sum1=0, sum2=0, sum3=0;
+    int count1, count2;
+
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+        sum1 += a[i];
     }
+    for(int i=0; i<n-1; i++){
+        scanf("%d", &b[i]);
+        sum2 += b[i];
+    }
+    for(int i=0; i<n-2; i++){
+        scanf("%d", &c[i]);
+        sum3+=c[i];
+    }
+    count1=sum1-sum2;
+    count2=sum2-sum3;
+
+    printf("%d %d\n", count1, count2);
+}
 
