ac7325811d575e2ff0e7021a15caace9d166c11c
Emon <emon22205101732@diu.edu.bd>
2024-02-22 00:22:50 +0600

    the second largest element is found.

--- bubble_sort.c
+++ bubble_sort.c
@@ -9,7 +9,6 @@ int main(){
             a[j]=a[j+1];
             a[j+1]=temp;
           }
-
         }
     }
     for(int i=0; i<=b; i++){
---
+++ labReport9.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int findSecondLargest(int arr[], int n) {
+    if (n < 2) {
+        printf("Array least two elements.");
+        return -1;
+    }
+    int comparisons = 0;
+    for(int i=0; i<n-1; i++){
+        int swapped =0;
+        for(int j=0; j<n-i-1; j++){
+            comparisons++;
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                swapped =1;
+            }
+            if(swapped>=9){
+                return arr[n-2];
+            }
+
+        }
+        if(!swapped){
+            break;
+        }
+    }
+    return arr[n-2];
+    
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[10];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int emon=findSecondLargest(arr, n);
+    printf("%d\n", emon);
+}
