8f3370def008857ccf726a45edf23e829336bf89
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2024-04-25 00:14:28 +0600

    Soldier and Bananas in codeforces

--- Lab-5,6,7,8/lab_5a.c
+++ Lab-5,6,7,8/lab_5a.c
@@ -16,11 +16,8 @@ int findSecondLargest(int arr[], int n) {
 }
 int main() {
     int n, i;
-    
     scanf("%d", &n);
     int arr[n];
-    
-   
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
